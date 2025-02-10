87783a90913bda43ff2e7a481c2e64e6baf8e858
Hyeok-Min-Kwon <prun000111@gmail.com>
2025-02-10 11:51:52 +0900

    dd

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const askQuestion = async (question) => {
   try {
-    const response = await axios.post('https://4.217.250.63/api/ask', { question });
+    const response = await axios.post("https://4.217.250.63/api/ask", { question });
     return response.data.answer;
   } catch (error) {
     throw new Error('Error fetching answer');
