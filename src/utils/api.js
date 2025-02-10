fe875706e9d0f94e576e8d1dc98666e9adb2bc8e
Hyeok-Min-Kwon <prun000111@gmail.com>
2025-02-10 13:37:56 +0900

    change the api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 
 export const askQuestion = async (question) => {
   try {
-    const response = await axios.post("https://4.217.250.63/api/ask", { question });
+    const response = await axios.post("https://4.217.250.63/api/ask", { question },{
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      withCredentials : true
+    }
+  );
     return response.data.answer;
   } catch (error) {
+    console.error("Axios Error", error);
     throw new Error('Error fetching answer');
   }
 };
