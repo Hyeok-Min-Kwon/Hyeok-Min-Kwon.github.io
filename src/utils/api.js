f3fd9e081c364f74ce81a6b9457db6a52cd3296a
Hyeok-Min-Kwon <prun000111@gmail.com>
2025-02-13 16:31:37 +0900

    port

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,14 +3,15 @@ import axios from 'axios';
 export const askQuestion = async (question) => {
   try {
     const response = await axios.post(
-      "https://4.217.250.8080/api/ask", 
+      "https://4.217.250:8080/api/ask", 
       { question },
       {
         headers: {
           "Content-Type" : "application/json"
         },
         withCredentials : true
-    }
+    },
+    {timeout : 50000}
   );
     return response.data.answer;
   } catch (error) {
