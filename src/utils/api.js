d59d56b8a42ab6162527063492f25cb79225efd7
Hyeok-Min-Kwon <prun000111@gmail.com>
2025-02-13 14:18:35 +0900

    dd

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const askQuestion = async (question) => {
   try {
     const response = await axios.post(
-      "https://4.217.250.63/api/ask", 
+      "http://4.217.250.8080/api/ask", 
       { question },
       {
         headers: {
